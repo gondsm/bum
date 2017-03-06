19c408f331eb41bc494d7c9eaf8c43fa6f8df92c
Gonçalo S. Martins <gmartins@isr.uc.pt>
2017-03-06 10:44:19 +0000

    Fix functions to user identity (except fusion)

--- user_model_tests.py
+++ user_model_tests.py
@@ -8,28 +8,35 @@ import probt
 import numpy as np
 import pprint
 
-# This dictionary will maintain the characteristics of the users.
+# This *dictionary* will maintain the characteristics of the users.
 # It is indexed by the tuple (tuple(evidence) + tuple(identity)), and
 # as such maintains the latest label for each combination of evidence
 # and identity. Uninitialized characteristics are kept as -1
 user_characteristics = []
 
 class characteristic_model:
-    def __init__(self, input_classes=[10,10], output_classes=10):
+    """ This class contains a full model of a single characteristic.
+    It is able to instantiate and fuse information, as well as to plot
+    its likelihoods, etc.
+
+    TODO: Normal usage
+    """
+    def __init__(self, input_classes=[10,10], output_classes=10, number_of_users=10):
         """ Initializes len(input_classes) input variables ranging from 1 to
         input_classes[i]. Initializes one output class ranging from 1 to
-        output_class.
+        output_class. Initializes the identity variable with number_of_users
+        possible values.
 
         Also initializes the prior distribution (as uniform) as well as uniform
         initial likelihoods.
         """
         # Define Variables
-        #self._E_1 = probt.plVariable("E_1", probt.plIntegerType(1, 10))
-        #self._E_2 = probt.plVariable("E_2", probt.plIntegerType(1, 10))
         self._C_1 = probt.plVariable("C_1", probt.plIntegerType(1, output_classes))
-        #self._input_variables = probt.plVariablesConjunction([self._E_1,self._E_2])
-        self._input_variables = probt.plVariablesConjunction([probt.plVariable("E_{}".format(i), probt.plIntegerType(1, input_classes[i])) for i in range(len(input_classes))])
-        #print(self._input_variables)
+        self._I = probt.plVariable("I", probt.plIntegerType(1, number_of_users))
+        input_variables = [probt.plVariable("E_{}".format(i), probt.plIntegerType(1, input_classes[i])) for i in range(len(input_classes))]
+        input_variables.append(self._I)
+        self._input_variables = probt.plVariablesConjunction(input_variables)
+        print(self._input_variables)
 
         # Define/fill in likelihood
         self._P_E_given_C_1 = probt.plDistributionTable(self._input_variables, self._C_1)
@@ -37,32 +44,34 @@ class characteristic_model:
         for i in range(output_classes):
             values[self._C_1] = i+1    
             self._P_E_given_C_1.push(probt.plUniform(self._input_variables), values)
-        #print("Initial likelihood", self._P_E_given_C_1)
 
         # Generate prior (don't worry, probt normalizes it for us)
-        self._P_C_1 = probt.plProbTable(self._C_1, [1]*10)
-        #print("Prior", self._P_C_1)
+        self._P_C_1 = probt.plProbTable(self._C_1, [1]*output_classes)
 
         # Lists for keeping results
         self._in_sequence = []
         self._class_sequence = []
         self._prob_sequence = []
+        self._identity_sequence = []
+
 
     def instantiate(self, evidence, identity):
         """ Instantiates the model, yielding a result class. """
+        # Define evidence
+        evidence_vals = probt.plValues(self._input_variables)
+        for i in range(len(evidence)):
+            evidence_vals[self._input_variables[i]] = evidence[i]
+        # Identity is always the last input variable:
+        evidence_vals[self._input_variables[len(evidence)]] = identity
+
         # Define joint distribution
         joint = probt.plJointDistribution(self._input_variables ^ self._C_1, self._P_C_1*self._P_E_given_C_1)
         
         # Define resulting conditional distribution
         P_C_1_given_E = joint.ask(self._C_1, self._input_variables)
-        
-        # Define evidence
-        evidence = probt.plValues(self._input_variables)
-        for i in range(len(evidence)):
-            evidence[self._input_variables[i]] = evidence[i]
-        
+
         # Instantiate/Compile into resulting distribution
-        result = P_C_1_given_E.instantiate(evidence)
+        result = P_C_1_given_E.instantiate(evidence_vals)
         result_class = result.n_best(1)[0].to_int()
         result_prob = result.tabulate()[1][result_class-1]
 
@@ -70,10 +79,13 @@ class characteristic_model:
         self._in_sequence.append(evidence)
         self._class_sequence.append(result_class)
         self._prob_sequence.append(result_prob)
+        self._identity_sequence.append(identity)
 
+        # Return result
         return result_class
 
-    def fuse(self, evidence, label, h, identity):
+
+    def fuse(self, label, evidence, identity, h):
         """ Updates the likelihood with a new example. 
         evidence: evidence vector
         label: class for which we're fusing
@@ -102,16 +114,17 @@ class characteristic_model:
         # Push into the distribution
         self._P_E_given_C_1.push(values, dist)
 
+
     def plot(label, evidence, identity):
         """ Plots the likelihood for the given evidence vector, identity label. """
         pass
 
+    
     def report(self):
         """ Print history of (input, output, probability) tuples. """
-        zipped = zip(self._in_sequence, self._class_sequence, self._prob_sequence)
-        print("(input, output, probability)")
+        zipped = zip(self._in_sequence, self._identity_sequence, self._class_sequence, self._prob_sequence)
+        print("(evidence, identity, output, probability)")
         pprint.pprint(list(zipped))
-        return self._class_sequence
 
 class user_simulator:
     """ A class for simulating a user. """
@@ -135,7 +148,7 @@ if __name__=="__main__":
     c1 = characteristic_model([10,10, 10], 10)
     
     for i in range(10):
-        result_class = c1.instantiate([1, 2, 3])
-        c1.fuse([1, 2, 3], result_class)
+        result_class = c1.instantiate([1, 2, 3], 1)
+        #c1.fuse(result_class, [1, 2, 3], 1, 1.0)
 
     c1.report()
