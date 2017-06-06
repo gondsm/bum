8d1238af7dbe54228cb81a4a2f8da7b697ca73f6
Gonçalo S. Martins <gmartins@isr.uc.pt>
2017-06-06 14:53:01 +0100

    Add stubs of the custom messages we'll be using.

--- scripts/bum_ros_node.py
+++ scripts/bum_ros_node.py
@@ -5,6 +5,7 @@ from __future__ import print_function
 
 # ROS
 import rospy
+import bum_ros.msg
 
 # Custom
 import bum_classes
@@ -13,4 +14,8 @@ import bum_classes
 if __name__=="__main__":
 	rospy.init_node('bum_ros_node')
 	rospy.loginfo("BUM ROS node started!")
+
+	#bum_ros.msg.Tuple()
+	#bum_ros.msg.Likelihood()
+
 	rospy.spin()
