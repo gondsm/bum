452ec189a6ee7819c0ce78d6e70d727761d4f19e
Gonçalo S. Martins <gmartins@isr.uc.pt>
2017-06-16 10:24:14 +0100

    Add a node stub able to import all necessary functions from other modules.

---
+++ bum_ros_conductor/scripts/bum_conductor.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python
+# Futures
+from __future__ import division
+from __future__ import print_function
+
+# Standard Lib
+import yaml
+
+# ROS
+import rospy
+from bum_ros.msg import Likelihood, Tuple, Evidence
+from conductor import gmu_functions as robot
