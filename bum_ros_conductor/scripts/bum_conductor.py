91a24455e55f0083a2b4f112295a6dcf2977565b
Gonçalo S. Martins <gmartins@isr.uc.pt>
2017-06-22 17:37:36 +0100

    Implement code to publish tuples on the other two characteristics.

--- bum_ros_conductor/scripts/bum_conductor.py
+++ bum_ros_conductor/scripts/bum_conductor.py
@@ -8,6 +8,8 @@ from __future__ import print_function
 import yaml
 import random
 import time
+import re
+import sys
 
 # ROS
 import rospy
@@ -17,9 +19,10 @@ from conductor import gmu_functions as robot
 
 # The volume steps we'll cycle through
 volume_steps = [40, 50, 60, 70, 75]
+current_volume = 2 # Starts in the middle of the scale
 
 # Variables for controlling the robot's "steps"
-initial_step = 2 # The robot will start in the middle
+current_step = 2 # The robot will start in the middle
 max_step = 4 # of a scale going from 0 to 4
 
 # The list of questions to be used in this test
@@ -28,11 +31,18 @@ questions = ["one",
              "three",
              "four"]
 questions_distance = ["Do you think I am speaking to you at the correct distance?"]
-questions_volume = ["Do you think I should speak at a different volume?"]
+questions_volume = ["Do you think I am speaking at the correct volume?"]
 
 # List of evidences of talkativeness gathered by the system
 ev_talk = []
 
+# Regexes for answer processing
+re_yes = ".*(yes|of course).*"
+re_no = ".*(no|not).*"
+re_louder = ".*(high|loud).*"
+re_quieter = ".*(quiet|low).*"
+re_closer = ".*(close|near).*"
+re_farther = ".*(away|far).*"
 
 def send_talkativeness_evidence(words, talk_time, ev):
     """ Stores and sends talkativeness evidence according to the words and 
@@ -45,9 +55,50 @@ def send_talkativeness_evidence(words, talk_time, ev):
     evidence_msg.values = ev
     evidence_msg.evidence_ids = ["Et{}".format(i) for i in reversed(range(len(ev)))]
     evidence_msg.user_id = 1
-    evidence_pub.publish(evidence_msg)
     rospy.loginfo("Publishing new talkativeness evidence:")
     rospy.loginfo(evidence_msg)
+    evidence_pub.publish(evidence_msg)
+
+
+def send_volume_tuple(val):
+    """ Sends a volume tuple of value val. """
+    tuple_msg = Tuple()
+    tuple_msg.char_id = "C1"
+    tuple_msg.characteristic = val
+    tuple_msg.evidence = []
+    tuple_msg.user_id = 1
+    tuple_msg.h = 0.001
+    tuple_msg.hard = True
+    rospy.loginfo("Publishing new volume tuple:")
+    rospy.loginfo(tuple_msg)
+    tuple_pub.publish(tuple_msg)
+
+
+def send_distance_tuple(val):
+    """ Sends a distance tuple of value val. """
+    tuple_msg = Tuple()
+    tuple_msg.char_id = "C2"
+    tuple_msg.characteristic = val
+    tuple_msg.evidence = []
+    tuple_msg.user_id = 1
+    tuple_msg.h = 0.001
+    tuple_msg.hard = True
+    rospy.loginfo("Publishing new distance tuple:")
+    rospy.loginfo(tuple_msg)
+    tuple_pub.publish(tuple_msg)
+
+
+def manual_transcription(query, kw_repeat=["repeat", "not understand"], lang="en-EN", txt_repeat="I will repeat.", replace=True, timeout=20, speech_time=False):
+    """ Emulates the speech recog function with raw_input. """
+    if type(query) is not list:
+        rospy.loginfo("Asking question: \"{}\".".format(query))
+    else:
+        rospy.loginfo("Asking question: \"{}\".".format(query[0]))
+    init = time.time()
+    text = raw_input().decode(sys.stdin.encoding or locale.getpreferredencoding(True))
+    talk_time = time.time() - init
+
+    return text, talk_time
 
 
 if __name__ == "__main__":
@@ -61,67 +112,55 @@ if __name__ == "__main__":
     tuple_pub = rospy.Publisher('bum/tuple', Tuple, queue_size=10)
     evidence_pub = rospy.Publisher('bum/evidence', Evidence, queue_size=10)
 
+    # Switch into transcription mode
+    #robot.ask_question = manual_transcription
+
     # Loop over all questions
     while questions:
         # Ask a question and receive answer
         rospy.loginfo("Asking a generic question...")
-        words, talk_time = robot.ask_question(questions, replace=False, timeout=1)
+        words, talk_time = robot.ask_question(questions, replace=False)
         send_talkativeness_evidence(words, talk_time, ev_talk)
 
         # Ask about volume
         rospy.loginfo("Asking volume...")
-        words, talk_time = robot.ask_question(questions_volume, timeout=1)
+        words, talk_time = robot.ask_question(questions_volume)
         send_talkativeness_evidence(words, talk_time, ev_talk)
+        if(re.search(re_no, words)):
+            words, talk_time = robot.ask_question("Should I be speaking louder or with a lower tone of voice?")
+            send_talkativeness_evidence(words, talk_time, ev_talk)
+            if(re.search(re_louder, words)):
+                if current_volume < len(volume_steps)-1:
+                    current_volume += 1
+                    robot.change_volume(volume_steps[current_volume])
+                else:
+                    robot.speak("I'm sorry, I cannot increase the volume any further.", lang="en-EN")
+            elif(re.search(re_quieter, words)):
+                if current_volume > 0:
+                    current_volume -= 1
+                    robot.change_volume(volume_steps[current_volume])
+                else:
+                    robot.speak("I'm sorry, I cannot lower the volume any further.", lang="en-EN")
+        send_volume_tuple(current_volume)
 
         # Ask about distance
         rospy.loginfo("Asking distance...")
-        words, talk_time = robot.ask_question(questions_distance, timeout=1)
+        words, talk_time = robot.ask_question(questions_distance)
         send_talkativeness_evidence(words, talk_time, ev_talk)
-
-    #rospy.sleep(3.)
-
-    # Publish some hard evidence
-    # tuple_msg = Tuple()
-    # tuple_msg.char_id = "C1"
-    # tuple_msg.characteristic = 4
-    # tuple_msg.evidence = [1,2,3]
-    # tuple_msg.user_id = 1
-    # tuple_msg.h = 0.001
-    # tuple_msg.hard = True
-    # tuple_pub.publish(tuple_msg)
-
-    # rospy.sleep(3.)
-
-    # tuple_msg.char_id = "C2"
-    # tuple_msg.characteristic = 7
-    # tuple_msg.evidence = [1,2]
-    # tuple_msg.user_id = 1
-    # tuple_msg.h = 0.001
-    # tuple_msg.hard = True
-    # tuple_pub.publish(tuple_msg)
-
-    # rospy.sleep(3.)
-
-    # tuple_msg.char_id = "C3"
-    # tuple_msg.characteristic = 9
-    # tuple_msg.evidence = [3]
-    # tuple_msg.user_id = 1
-    # tuple_msg.h = 0.001
-    # tuple_msg.hard = True
-    # tuple_pub.publish(tuple_msg)
-
-    # rospy.sleep(3.)
-
-    # # Publish some regular evidence
-    # evidence_msg = Evidence()
-    # evidence_msg.values = [1,2,3]
-    # evidence_msg.evidence_ids = ["E1", "E2", "E3"]
-    # evidence_msg.user_id = 1
-    # evidence_pub.publish(evidence_msg)
-
-    #robot.step_forward()
-    #robot.step_forward()
-    #robot.step_forward(True)
-    #robot.step_forward(True)
-
-    
+        if(re.search(re_no, words)):
+            words, talk_time = robot.ask_question("Should I be closer or farther away from you?")
+            send_talkativeness_evidence(words, talk_time, ev_talk)
+            if(re.search(re_farther, words)):
+                if current_step < max_step-1:
+                    current_step += 1
+                    robot.step_forward(reverse=True)
+                else:
+                    robot.speak("I'm sorry, I cannot go further back.", lang="en-EN")
+            elif(re.search(re_closer, words)):
+                if current_step > 0:
+                    current_step -= 1
+                    robot.step_forward()
+                else:
+                    robot.speak("I'm sorry, I cannot go further forward.", lang="en-EN")
+        send_distance_tuple(current_step)
+   
